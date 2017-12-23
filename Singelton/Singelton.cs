79c9f2d4a7be3665d26bc931d02a21ae0b8fd578
tetiana.kozlovska15 <tetiana.kozlovska15@gmail.com>
2017-12-23 23:53:59 +0200

    Add project files.

---
+++ Singelton/Singelton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singelton
+{
+   public class Singelton
+    {
+        public Singelton singelton { get; set; }
+        private Singelton() { }
+        public Singelton GetInstanse() {
+            if (singelton == null)
+            {
+                singelton = new Singelton();
+            }
+            return singelton;
+        }
+    }
+}
